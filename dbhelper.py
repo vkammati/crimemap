f1844b784c445c168abbf25f92f459be20cb5948
vkammati <kvk5592007@gmail.com>
2022-08-31 18:42:53 +0530

    Add more functionality to crimemap

---
+++ dbhelper.py
@@ -0,0 +1,44 @@
+import mariadb
+import dbconfig
+
+class DBHelper:
+
+    def connect(self):
+        return mariadb.connect(
+            user=dbconfig.db_user,
+            password=dbconfig.db_password,
+            host=dbconfig.host,
+            port=dbconfig.port,
+            database=dbconfig.database
+        )
+
+    def get_all_inputs(self):
+        connection = self.connect()
+        try:
+            query = "SELECT description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                return cursor.fetchall()
+        finally:
+            connection.close()
+
+    def add_input(self, data):
+        connection = self.connect()
+        try:
+            # The following introduces a deliberate security flaw. See section on SQL injection below
+            query = "INSERT INTO crimes (description) VALUES (%s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (data,))
+                connection.commit()
+        finally:
+            connection.close()
+
+    def clear_all(self):
+        connection = self.connect()
+        try:
+            query = "DELETE FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+        finally:
+            connection.close()
