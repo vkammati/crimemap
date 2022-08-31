7a97afaaeab724630387c18bcae92be1f0b7a578
vkammati <kvk5592007@gmail.com>
2022-08-31 14:39:33 +0530

    correct errors and make program workable

--- dbconfig.py
+++ dbconfig.py
@@ -2,6 +2,6 @@ import os
 
 db_user=os.environ.get('MARIADB_USER_ID')
 db_password=os.environ.get('MARIADB_PASSWORD')
-host=os.environ.get('MARIADB_HOST'),
-port=os.environ.get('MARIADB_PORT'),
+host=os.environ.get('MARIADB_HOST')
+port=int(os.environ.get('MARIADB_PORT'))
 database=os.environ.get('MARIADB_DATABASE')
