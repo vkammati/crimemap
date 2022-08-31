43520c0f80ce7d9ee81c87abe7c5b85b52383a40
vkammati <kvk5592007@gmail.com>
2022-08-31 14:13:34 +0530

    database setup script

---
+++ db_setup.py
@@ -0,0 +1,32 @@
+import mariadb
+import sys
+import dbconfig
+
+try:
+    conn = mariadb.connect(
+        user=dbconfig.db_user,
+        password=dbconfig.db_password,
+        host=dbconfig.host,
+        port=dbconfig.port,
+        databa=dbconfig.database
+    )
+except mariadb.Error as e:
+    print("Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+try:
+    with conn.cursor() as cursor:
+        sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
+                    id int NOT NULL AUTO_INCREMENT,
+                    latitude FLOAT(10,6),
+                    longitude FLOAT(10,6),
+                    date DATETIME,
+                    category VARCHAR(50),
+                    description VARCHAR(1000),
+                    updated_at TIMESTAMP,
+                    PRIMARY KEY (id)
+                    )"""
+        cursor.execute(sql);
+    conn.commit()
+finally:
+    conn.close()
---
+++ dbconfig.py
@@ -0,0 +1,5 @@
+db_user='root'
+db_password='root'
+host='127.0.0.1',
+port=3306,
+database='crimemap'
